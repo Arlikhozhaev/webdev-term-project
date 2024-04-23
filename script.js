2289c91589a82111bf02ca8560c466d94fd77785
Abdu Alim <arlikhozhaevca@gmail.com>
2024-04-22 17:21:24 -0700

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let navButton = document.getElementById("jsbutton");
+
+navButton.addEventListener("click", function(event){
+    navButton.textContent = "Processing..."
+    navButton.style.width = "max-content"
+});
+ 
+
